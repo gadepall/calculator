d4de3fdae34da5fee6d74e923f01c4160bf387ca
Subhodeep Chakraborty <subhoboy@vivaldi.net>
2026-05-12 12:37:11 +0530

    Review fixes part 3
    Final hardware analysis

--- analysis/fpu.c
+++ analysis/fpu.c
@@ -1,8 +1,47 @@
-#include <avr/sleep.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <stdio.h>
+
+// Simple UART output
+int uart_putchar(char c, FILE *stream) {
+    if (c == '\n') uart_putchar('\r', stream);
+    while (!(UCSR0A & (1 << UDRE0)));
+    UDR0 = c;
+    return 0;
+}
+FILE uart_output = FDEV_SETUP_STREAM(uart_putchar, NULL, _FDEV_SETUP_WRITE);
+
+// Algo code
+volatile float c;
+volatile float a = 5.0f, b = 3.0f;
+void div() {
+    c=a+b;
+}
 
 int main(void) {
-    volatile int a = 5, b = 3, c;
-    c = a / b;
-    sleep_mode();
-    return 0;
+    uint16_t start_cycles, end_cycles, total_cycles;
+
+    UCSR0B = (1 << TXEN0); 
+    stdout = &uart_output;
+
+    // Initialize Timer1 (Normal mode, Prescaler = 1)
+    TCCR1A = 0;
+    TCCR1B = (1 << CS10); 
+
+    printf("Starting Benchmark...\n");
+
+    TCNT1 = 0; 
+    start_cycles = TCNT1; 
+
+    div();
+
+    end_cycles = TCNT1; 
+
+    total_cycles = end_cycles - start_cycles;
+    
+    printf("Algorithm took: %u cycles\n", total_cycles);
+    
+    // Sleep
+    cli();
+    while(1);
 }
--- analysis/hardware.c
+++ analysis/hardware.c
@@ -1,4 +1,17 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <stdio.h>
 
+// Simple UART output
+int uart_putchar(char c, FILE *stream) {
+    if (c == '\n') uart_putchar('\r', stream);
+    while (!(UCSR0A & (1 << UDRE0)));
+    UDR0 = c;
+    return 0;
+}
+FILE uart_output = FDEV_SETUP_STREAM(uart_putchar, NULL, _FDEV_SETUP_WRITE);
+
+// Algo code
 float f(float x) {
     return 1.0f / x;
 }
@@ -8,7 +21,6 @@ volatile float y = 0.0f;
 volatile float h = 0.06f;
 volatile float result;
 
-#if 0
 float backward_euler(float x, float y, float h) {
     return y + h * f(x + h);
 }
@@ -25,16 +37,35 @@ float rk4(float x, float y, float h) {
 float trapezoidal(float x, float y, float h) {
     return y + (h / 2.0f) * (f(x) + f(x + h));
 }
-#endif
 
 float forward_euler(float x, float y, float h) {
     return y + h * f(x);
 }
-
 int main(void) {
-    result = forward_euler(x,y,h);
-    while(1) {
-        asm volatile("nop"); 
-    }
-    return 0;
+    uint16_t start_cycles, end_cycles, total_cycles;
+
+    UCSR0B = (1 << TXEN0); 
+    stdout = &uart_output;
+
+    // Initialize Timer1 (Normal mode, Prescaler = 1)
+    TCCR1A = 0;
+    TCCR1B = (1 << CS10); 
+
+    printf("Starting Benchmark...\n");
+
+    TCNT1 = 0; 
+    start_cycles = TCNT1; 
+
+    result = rk4(x,y,h);
+
+    end_cycles = TCNT1; 
+
+    total_cycles = end_cycles - start_cycles;
+    
+    printf("Algorithm took: %u cycles\n", total_cycles);
+    
+    // Sleep
+    cli();
+    while(1);
 }
+
